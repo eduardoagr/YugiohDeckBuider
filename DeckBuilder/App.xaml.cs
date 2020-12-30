5767793b4b9ee4f417627279b71a45e61da43054
eduardoagr <egomezr@outlook.com>
2020-12-30 18:59:41 +0100

    StartUp Project

---
+++ DeckBuilder/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DeckBuilder {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart() {
+        }
+
+        protected override void OnSleep() {
+        }
+
+        protected override void OnResume() {
+        }
+    }
+}
---
+++ DeckBuilder/View/DecksCollectionPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DeckBuilder.View {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DecksCollectionPage : ContentPage {
+        public DecksCollectionPage() {
+            InitializeComponent();
+        }
+    }
+}
